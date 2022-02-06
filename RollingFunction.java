980cfd5ac5e21494fbc7d076da2a3cf5215bd51d
renzzyy <99091376+renzzyy@users.noreply.github.com>
2022-02-06 00:03:06 -0500

    bug fixes and test removal

---
+++ RollingFunction.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+import java.util.ArrayList;
+
+/**
+ * This class acts as the back end calculator of the dice
+ * @author Lauren
+ *
+ */
+public class RollingFunction {
+	private ArrayList<Object> num = new ArrayList<>();
+	private int total;
+	private int roll = 0;
+	
+	public void roll (int numDice, int typeDice) {
+		for (int i = 0; i < numDice; i++) {
+			roll = (int)((Math.random() * (typeDice)) + 1);	
+			total = total + roll;
+			num.add(roll);
+		}
+	}
+	
+	public ArrayList<Object> rollArray() {
+		return num;
+	}
+	
+	public int rollTotal() {
+		return total;
+	}
+
+}
